9f627cf224c8e43d794f96128b564c5dfea19ef3
Faza Affandi <affandifaza@gmail.com>
2023-10-25 13:16:18 +0700

    Add some model

--- lib/model/transaksi.dart
+++ lib/model/transaksi.dart
@@ -2,12 +2,11 @@ class Transaksi {
   final String nama;
   final String tanggal;
   final String nominal;
-  final bool jenis;
+  final bool jenis; // true = +   false = -
   final String kategori;
   final String kategoriGambar;
   final String deskripsi;
   final String changes;
-
 
   Transaksi({
     required this.nama,
@@ -37,9 +36,39 @@ var transaksiList = [
     tanggal: '5 Oktober 2023',
     nominal: 'Rp 1.000.000',
     jenis: true,
-    kategori: 'Tranfer (Masuk)',
+    kategori: 'Transfer (Masuk)',
     kategoriGambar: 'assets/TransferMasukCategory.png',
     deskripsi: 'Ini deskripsi',
     changes: '+Rp1.000.000',
+  ),
+  Transaksi(
+    nama: 'Tagihan Listrik',
+    tanggal: '25 September 2023',
+    nominal: 'Rp 350.000',
+    jenis: true,
+    kategori: 'Tagihan',
+    kategoriGambar: 'assets/TagihanCategory.png',
+    deskripsi: 'Ini deskripsi',
+    changes: '-Rp350.000',
+  ),
+  Transaksi(
+    nama: 'Transfer Mandiri',
+    tanggal: '10 Juli 2023',
+    nominal: 'Rp 500.000',
+    jenis: false,
+    kategori: 'Transfer (Keluar)',
+    kategoriGambar: 'assets/TransferKeluarCategory.png',
+    deskripsi: 'Ini deskripsi',
+    changes: '-Rp500.000',
+  ),
+  Transaksi(
+    nama: 'Tiket Pesawat',
+    tanggal: '15 April 2023',
+    nominal: 'Rp 1.500.000',
+    jenis: false,
+    kategori: 'Tiket',
+    kategoriGambar: 'assets/TiketCategory.png',
+    deskripsi: 'Ini deskripsi',
+    changes: '+Rp1.500.000',
   ),
 ];
